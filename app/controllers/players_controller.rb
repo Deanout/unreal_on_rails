6b5379d07e640567dd5f379268bf4fdf8586016c
Dean DeHart <DeHartDean@gmail.com>
2023-02-15 06:34:45 -0500

    Fix params

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,5 @@
 class PlayersController < ApplicationController
-  before_action :set_player, only: %i[ show update destroy ]
+  before_action :set_player, only: %i[show update destroy]
 
   # GET /players
   def index
@@ -39,13 +39,14 @@ class PlayersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_player
-      @player = Player.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def player_params
-      params.require(:player).permit(:x, :y, :z)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_player
+    @player = Player.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def player_params
+    params.fetch({}).permit(:x, :y, :z)
+  end
 end
